9b035f0bc4f3de58a382fd54d09b52b5c9df5d97
thomasmfx <thomasmoisesf@gmail.com>
2024-12-18 14:20:29 -0300

    🌟 feat: add cart items's count and improve style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { Home, ShoppingBag, ShoppingCart, User } from 'react-feather'
 
+const iconSize = 21
+
 const StyledNav = styled.nav`
   display: grid;
   grid-template-columns: 1fr 2fr 1fr;
@@ -19,8 +21,9 @@ const Wrapper = styled.div`
 `
 
 const Button = styled.button`
-  background: ${(props) => (props.selected ? '#dff0ff' : 'none')};
-  color: ${(props) => (props.selected ? '#188efe' : 'black')};
+  background: ${(props) =>
+    props.selected ? 'var(--color-secondary)' : 'none'};
+  color: ${(props) => (props.selected ? 'var(--color-primary)' : 'black')};
   border: none;
   display: flex;
   justify-content: center;
@@ -28,28 +31,60 @@ const Button = styled.button`
   gap: 0.5em;
   font-family: Montserrat;
   cursor: pointer;
-  font-weight: 500;
   padding: 0.4em 0.8em;
   font-weight: 500;
   border-radius: 5px;
   width: 140px;
 
   &:hover {
-    color: #188efe;
+    color: ${'var(--color-primary)'};
   }
 `
 
-const CartButton = styled(Button)`
+const StyledCartButton = styled(Button)`
   border-radius: 50%;
   padding: 0.7em;
   width: 46.5px;
+  position: relative;
+`
+
+const ItemsCount = styled.div`
+  width: 16.5px;
+  height: 16.5px;
+  position: absolute;
+  top: 13%;
+  right: 13%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${'var(--color-primary)'};
+  border-radius: 50%;
+  color: white;
+  font-size: 10px;
+  font-weight: bold;
+  transform: translate(-7%, 10%);
 `
 
 const Logo = styled.p`
   font-size: 1.8rem;
   font-weight: bold;
-  color: #188efe;
+  color: ${'var(--color-primary)'};
 `
+
+function CartButton({ itemsCount, selected }) {
+  return (
+    <StyledCartButton selected={selected}>
+      <ShoppingCart
+        size={iconSize}
+        style={{ transform: 'translate(-7%, 10%)' }}
+      />
+      <ItemsCount>
+        <p>{itemsCount}</p>
+      </ItemsCount>
+    </StyledCartButton>
+  )
+}
 
 function Nav({ currentRoute = '/' }) {
   return (
@@ -60,30 +95,33 @@ function Nav({ currentRoute = '/' }) {
       <Wrapper>
         <Link to={'/'} style={{ textDecoration: 'none' }}>
           <Button selected={currentRoute === '/'}>
-            <Home size={21} />
+            <Home size={iconSize} />
             <p>Home</p>
           </Button>
         </Link>
         <Link to={'/about-us'} style={{ textDecoration: 'none' }}>
           <Button selected={currentRoute === 'about-us'}>
-            <User size={21} />
+            <User size={iconSize} />
             <p>About Us</p>
           </Button>
         </Link>
         <Link to={'/shop'} style={{ textDecoration: 'none' }}>
           <Button selected={currentRoute === 'shop'}>
-            <ShoppingBag size={21} />
+            <ShoppingBag size={iconSize} />
             <p>Shop</p>
           </Button>
         </Link>
       </Wrapper>
       <Link to={'/cart'} style={{ textDecoration: 'none' }}>
-        <CartButton selected={currentRoute === 'cart'}>
-          <ShoppingCart size={21} />
-        </CartButton>
+        <CartButton itemsCount="0" selected={currentRoute === 'cart'} />
       </Link>
     </StyledNav>
   )
+}
+
+CartButton.propTypes = {
+  itemsCount: PropTypes.string,
+  selected: PropTypes.bool,
 }
 
 Nav.propTypes = {
