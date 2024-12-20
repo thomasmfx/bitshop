8cb5175b257980fa2e71ac6b28756aeaf92b7097
thomasmfx <thomasmoisesf@gmail.com>
2024-12-19 21:58:18 -0300

    🦄 refactor: export Logo to own module

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const StyledLogo = styled.p`
+  font-size: ${(props) => (props.fontSize ? props.fontSize : '2rem')};
+  font-weight: bold;
+  color: ${'var(--color-primary)'};
+`
+
+function Logo({ size, removeDot }) {
+  return <StyledLogo fontSize={size}>bitShop {!removeDot && '•'}</StyledLogo>
+}
+
+Logo.propTypes = {
+  size: PropTypes.number,
+  removeDot: PropTypes.bool,
+}
+
+export default Logo
