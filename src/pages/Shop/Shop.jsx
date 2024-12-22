57f68e4ea30d46906242b29528bbd612c2f76488
thomasmfx <thomasmoisesf@gmail.com>
2024-12-21 21:52:06 -0300

    🌟 feat: finish Shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,5 +1,51 @@
-import Nav from '../../components/Nav/Nav'
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import ProductCard from '../../components/ProductCard/ProductCard'
+import fetchProducts from '../../utils/fetchProducts'
+
+const Main = styled.main`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5em;
+  padding: 6em;
+  padding-bottom: 8em;
+`
+
+const ProductsContainer = styled.div`
+  grid-column: 2 / 3;
+  display: grid;
+  grid-template-columns: repeat(3, min-content);
+  justify-content: center;
+  gap: 5em;
+`
 
 export default function Shop() {
-  return <Nav currentRoute="shop" />
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then(data => setProducts(data))
+  }, [])
+
+  return (
+    <>
+      <Header currentRoute="shop" />
+      <Main>
+        <ProductsContainer>
+          {products.map((product) =>
+            <ProductCard 
+              key={product.id} 
+              product={product}
+              isNew={(product.id % 5) === 0}
+            />
+          )}
+        </ProductsContainer>
+      </Main>
+      <Footer />
+    </>
+  )
 }
