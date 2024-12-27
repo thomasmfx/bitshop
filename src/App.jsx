41995f6bb5c9d99f48462dc3d1e5e41a9b333859
thomasmfx <thomasmoisesf@gmail.com>
2024-12-27 15:38:13 -0300

    🌟 feat: add validation to the quantity of items in cart

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,20 @@ function App() {
   const [cartProducts, setCartProducts] = useState([])
 
   function handleAddProduct(product, quantity) {
+    if (cartProducts.length === 99) {
+      alert(
+        'Cart limit of 99 items reached. Please complete your current purchase to add more items.',
+      )
+      return
+    }
+
+    if (cartProducts.length + quantity > 99) {
+      alert(
+        'This action exceeds the cart limit of 99 items. Please adjust the quantity or complete your current purchase.',
+      )
+      return
+    }
+
     if (quantity <= 0) return
     setCartProducts((prev) => [...prev, ...Array(quantity).fill(product)])
   }
