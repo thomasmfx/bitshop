0150c72bb38a1d00aeef27361169d9aaf0f14fbf
thomasmfx <thomasmoisesf@gmail.com>
2025-01-08 14:20:49 -0300

    🌟 feat: add responsiveness to Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,19 +1,44 @@
+import { Circle } from 'react-feather'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const StyledLogo = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 0.2em;
   font-size: ${(props) => (props.fontSize ? props.fontSize : '2rem')};
   font-weight: bold;
   color: ${({ theme }) => theme.colors.brandPrimary};
+
+  > svg {
+    min-width: 10px;
+    width: 10px;
+  }
+
+  @media (max-width: 642px) {
+    > span {
+      display: none;
+    }
+
+    > svg {
+      width: 18px;
+    }
+  }
 `
 
-function Logo({ size, removeDot }) {
-  return <StyledLogo fontSize={size}>bitShop {!removeDot && '•'}</StyledLogo>
+const Span = styled.span``
+
+function Logo({ size }) {
+  return (
+    <StyledLogo fontSize={size}>
+      <Span>bitShop</Span>
+      <Circle fill="#188efe" />
+    </StyledLogo>
+  )
 }
 
 Logo.propTypes = {
   size: PropTypes.string,
-  removeDot: PropTypes.bool,
 }
 
 export default Logo
