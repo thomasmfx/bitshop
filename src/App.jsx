795408429ef5e9c88be1ffc43b44ed59e791e03e
thomasmfx <thomasmoisesf@gmail.com>
2025-01-08 22:16:34 -0300

    🌟 feat: add "scroll to top" behavior on route changing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import ScrollToTop from './utils/scrollToTop'
 
 function App() {
   const CART_LIMIT = 99
@@ -86,9 +87,11 @@ function App() {
 
   return (
     <>
-      <Header cartProductsCount={getTotalItems()} />
-      <Outlet context={cartContext} />
-      <Footer />
+      <ScrollToTop>
+        <Header cartProductsCount={getTotalItems()} />
+        <Outlet context={cartContext} />
+        <Footer />
+      </ScrollToTop>
     </>
   )
 }
