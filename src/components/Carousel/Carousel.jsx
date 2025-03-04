e47e379d4245f77061055bf17704e1c32eceb21a
thomasmfx <thomasmoisesf@gmail.com>
2025-03-04 16:58:39 -0300

    🌟 feat: create Carousel component

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import { ChevronLeft, ChevronRight } from 'react-feather'
+import * as S from './Carousel.styles'
+
+const ARROW_SIZE = 32
+const ARROW_COLOR = '#6c757d'
+
+export default function Carousel({ children }) {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [length, setLength] = useState(children.length)
+
+  useEffect(() => {
+    setLength(children.length)
+  }, [children])
+
+  function next() {
+    if (currentIndex < length - 1) {
+      setCurrentIndex((prevState) => prevState + 1)
+    }
+  }
+
+  function prev() {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevState) => prevState - 1)
+    }
+  }
+
+  function jumpImage(index) {
+    setCurrentIndex(index)
+  }
+
+  return (
+    <S.CarouselContainer>
+      <S.CarouselWrapper>
+        {currentIndex > 0 && (
+          <S.LeftArrow onClick={prev}>
+            <ChevronLeft size={ARROW_SIZE} color={ARROW_COLOR} />
+          </S.LeftArrow>
+        )}
+        <S.CarouselContentWrapper>
+          <S.CarouselContent
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {children}
+          </S.CarouselContent>
+        </S.CarouselContentWrapper>
+        {currentIndex < length - 1 && (
+          <S.RightArrow onClick={next}>
+            <ChevronRight size={ARROW_SIZE} color={ARROW_COLOR} />
+          </S.RightArrow>
+        )}
+        <S.DotsWrapper>
+          {children.map((child, index) => (
+            <S.Dot
+              key={index}
+              currentIndex={currentIndex === index}
+              onClick={() => jumpImage(index)}
+            />
+          ))}
+        </S.DotsWrapper>
+      </S.CarouselWrapper>
+    </S.CarouselContainer>
+  )
+}
---
+++ src/components/Carousel/Carousel.styles.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components'
+
+export const CarouselContainer = styled.div`
+  display: flex;
+  width: 400px;
+  flex-direction: column;
+`
+
+export const CarouselWrapper = styled.div`
+  width: 400px;
+  display: flex;
+  position: relative;
+`
+
+export const CarouselContentWrapper = styled.div`
+  width: 400px;
+  overflow: hidden;
+  height: 100%;
+`
+
+export const CarouselContent = styled.div`
+  display: flex;
+  transition: all 250ms linear;
+  -ms-overflow-style: none; /* hide scrollbar in IE and Edge */
+  scrollbar-width: none; /* hide scrollbar in Firefox */
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  & > * {
+    width: 100%;
+    height: 400px;
+    flex-shrink: 0;
+    flex-grow: 1;
+  }
+`
+
+const Arrow = styled.button`
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  background: none;
+  border: none;
+  display: grid;
+  align-items: center;
+  cursor: pointer;
+`
+
+export const LeftArrow = styled(Arrow)`
+  left: 0px;
+
+  & > * {
+    justify-self: start;
+  }
+`
+
+export const RightArrow = styled(Arrow)`
+  right: 0px;
+
+  & > * {
+    justify-self: end;
+  }
+`
+
+export const DotsWrapper = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 0;
+  right: 0;
+  margin-inline: auto;
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+`
+
+export const Dot = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: ${(props) => (props.currentIndex ? '#6c757d' : '')};
+  border: 2px solid #6c757d;
+  border-radius: 50%;
+  cursor: pointer;
+`
