71cd1d79f0a923e46b98c20912e08cb11ab7213b
thomasmfx <thomasmoisesf@gmail.com>
2025-03-16 16:25:28 -0300

    🧪 test: write ProductCard tests

---
+++ src/components/ProductCard/ProductCard.test.jsx
@@ -0,0 +1,72 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { useOutletContext } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import { TestWrapper, LocationDisplay } from '../../tests/testUtils'
+import ProductCard from './ProductCard'
+import mockProduct from '../../__mocks__/mockProduct'
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  const actual = await importOriginal(); // Importa as exportações originais
+  return {
+    ...actual, // Mantém todas as exportações originais
+    useOutletContext: vi.fn(), // Mocka apenas o useOutletContext
+  };
+});
+
+describe("ProductCard", () => {
+  describe("should render", () => {
+    useOutletContext.mockReturnValue({
+      addProduct: vi.fn(),
+    });
+
+    it("product title", () => {
+      render(
+        <TestWrapper>
+          <ProductCard product={mockProduct}/>
+        </TestWrapper>
+      )
+  
+      expect(screen.getByText(mockProduct.title)).toBeInTheDocument()
+    })
+    it("product price", () => {
+      render(
+        <TestWrapper>
+          <ProductCard product={mockProduct}/>
+        </TestWrapper>
+      )
+  
+      expect(screen.getByText(mockProduct.price)).toBeInTheDocument()
+    })
+    it("product rating", () => {
+      render(
+        <TestWrapper>
+          <ProductCard product={mockProduct}/>
+        </TestWrapper>
+      )
+  
+      expect(screen.getByTestId('Rating')).toBeInTheDocument()
+    })
+  })
+  
+  it('should redirect to product page on user click', async () => {
+    const user = userEvent.setup()
+    const expectedRoute = `/shop/${mockProduct.id}`
+    const ariaLabel = 'Product page'
+
+    render(
+      <TestWrapper>
+        <ProductCard product={mockProduct}/>
+        <LocationDisplay />
+      </TestWrapper>
+    )
+
+    const routerLink = screen.getByRole('link', { name: ariaLabel })
+    await user.click(routerLink)
+
+    expect(routerLink).toHaveAttribute('aria-label', ariaLabel)
+    expect(screen.getByTestId('location-display')).toHaveTextContent(
+      expectedRoute,
+    )
+  })
+})
