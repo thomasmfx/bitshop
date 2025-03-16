2a960a242805547f997e707522223081ab97c135
thomasmfx <thomasmoisesf@gmail.com>
2025-03-16 16:21:32 -0300

    🌟 feat: add product mock

---
+++ src/__mocks__/mockProduct.js
@@ -0,0 +1,63 @@
+const mockProduct = {
+  "id": 80,
+  "title": "Huawei Matebook X Pro",
+  "description": "The Huawei Matebook X Pro is a slim and stylish laptop with a high-resolution touchscreen display, offering a premium experience for users on the go.",
+  "category": "laptops",
+  "price": 1399.99,
+  "discountPercentage": 15.25,
+  "rating": 4.62,
+  "stock": 34,
+  "tags": [
+    "laptops"
+  ],
+  "brand": "Huawei",
+  "sku": "U2CBNNB0",
+  "weight": 9,
+  "dimensions": {
+    "width": 22.11,
+    "height": 11.11,
+    "depth": 23.07
+  },
+  "warrantyInformation": "2 year warranty",
+  "shippingInformation": "Ships in 2 weeks",
+  "availabilityStatus": "In Stock",
+  "reviews": [
+    {
+      "rating": 4,
+      "comment": "Great value for money!",
+      "date": "2024-05-23T08:56:21.622Z",
+      "reviewerName": "Samantha Howard",
+      "reviewerEmail": "samantha.howard@x.dummyjson.com"
+    },
+    {
+      "rating": 2,
+      "comment": "Would not buy again!",
+      "date": "2024-05-23T08:56:21.622Z",
+      "reviewerName": "Daniel Cook",
+      "reviewerEmail": "daniel.cook@x.dummyjson.com"
+    },
+    {
+      "rating": 5,
+      "comment": "Very satisfied!",
+      "date": "2024-05-23T08:56:21.622Z",
+      "reviewerName": "Brayden Fleming",
+      "reviewerEmail": "brayden.fleming@x.dummyjson.com"
+    }
+  ],
+  "returnPolicy": "60 days return policy",
+  "minimumOrderQuantity": 1,
+  "meta": {
+    "createdAt": "2024-05-23T08:56:21.622Z",
+    "updatedAt": "2024-05-23T08:56:21.622Z",
+    "barcode": "1232723732298",
+    "qrCode": "https://assets.dummyjson.com/public/qr-code.png"
+  },
+  "images": [
+    "https://cdn.dummyjson.com/products/images/laptops/Huawei%20Matebook%20X%20Pro/1.png",
+    "https://cdn.dummyjson.com/products/images/laptops/Huawei%20Matebook%20X%20Pro/2.png",
+    "https://cdn.dummyjson.com/products/images/laptops/Huawei%20Matebook%20X%20Pro/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/laptops/Huawei%20Matebook%20X%20Pro/thumbnail.png"
+}
+
+export default mockProduct
