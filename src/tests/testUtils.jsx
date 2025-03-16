b0a1215393ea21d35adf8a063ae1f4db8820dc5d
thomasmfx <thomasmoisesf@gmail.com>
2025-03-16 16:22:01 -0300

    🦄 refactor: improve testUtils

--- src/tests/testUtils.jsx
+++ src/tests/testUtils.jsx
@@ -1,29 +1,32 @@
-import isPropValid from '@emotion/is-prop-valid'
-import PropTypes from 'prop-types'
-import { useLocation, BrowserRouter } from 'react-router-dom'
-import { ThemeProvider, StyleSheetManager } from 'styled-components'
-import theme from '../styles/theme'
-import GlobalStyles from '../styles/globals'
+import isPropValid from '@emotion/is-prop-valid';
+import PropTypes from 'prop-types';
+import { useLocation, MemoryRouter } from 'react-router-dom';
+import { ThemeProvider, StyleSheetManager } from 'styled-components';
+import theme from '../styles/theme';
+import GlobalStyles from '../styles/globals';
 
 function TestWrapper({ children }) {
   return (
     <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <BrowserRouter>{children}</BrowserRouter>
+        <MemoryRouter>
+          {children}
+        </MemoryRouter>
       </ThemeProvider>
     </StyleSheetManager>
-  )
+  );
 }
 
 function LocationDisplay() {
-  const location = useLocation()
+  const location = useLocation();
 
-  return <div data-testid="location-display">{location.pathname}</div>
+  return <div data-testid="location-display">{location.pathname}</div>;
 }
 
 TestWrapper.propTypes = {
   children: PropTypes.node,
-}
+  withOutlet: PropTypes.bool, // Nova prop para controlar o uso do Outlet
+};
 
-export { TestWrapper, LocationDisplay }
+export { TestWrapper, LocationDisplay };
